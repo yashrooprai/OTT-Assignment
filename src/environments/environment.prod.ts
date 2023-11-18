9b09297c093e981f437705883b86f521bbc4a1c0
Yash Roop Rai <yashrooprai09@gmail.com>
2023-11-18 22:44:57 +0530

    -prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBJ7AOVybp8PH2BP3vXQf_hCtbQEFKxE-A",
+    authDomain: "durable-stack-405413.firebaseapp.com",
+    projectId: "durable-stack-405413",
+    storageBucket: "durable-stack-405413.appspot.com",
+    messagingSenderId: "159091541367",
+    appId: "1:159091541367:web:a3e324d4be352f66474d72",
+    measurementId: "G-VGLH2KL4HN"
+  },
+  db: "https://durable-stack-405413-default-rtdb.firebaseio.com/",
+  tmdbApi: "08cc33bd5ae3a747598ce2ad84376e66"
 };
